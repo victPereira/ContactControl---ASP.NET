9f6a5d761ea90514e5acbad1573bf324a95254f3
Victor Pereira <vic.torpereira@hotmail.com>
2023-09-15 20:14:13 +0100

    Adicionar arquivos de projeto.

---
+++ ContactControl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactControl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
